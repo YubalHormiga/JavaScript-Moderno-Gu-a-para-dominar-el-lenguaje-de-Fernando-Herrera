636dc68f54c9a724e3baea25b7f4fa4bda00ffe6
YubalHormiga <yubalhh@gmail.com>
2023-04-24 11:34:13 +0200

    Todo-App finished

--- 10-Todo-App/src/store/todo.store.js
+++ 10-Todo-App/src/store/todo.store.js
@@ -1,27 +1,117 @@
-import { Todo } from "../todos/models/todo.model";
+import { Todo } from '../todos/models/todo.model';
 
-
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'Completed',
-    Pending:'Pending'
-
+    Pending: 'Pending'
 }
 
 const state = {
-    todos:[
-        new Todo('Piedra de alma'),
-        new Todo('Piedra de infinito'),
-        new Todo('Piedra de tiempo'),
+    todos: [
+        new Todo('Pieda del alma'),
+        new Todo('Pieda del espacio'),
+        new Todo('Pieda del tiempo'),
+        new Todo('Pieda del poder'),
+        new Todo('Pieda del realidad'),
     ],
-    filter: Filters.All
+    filter: Filters.All,
 }
 
-const initStore = ()=>{
-    console.log(state)
-    console.log('initStore 🐱‍🐉')
+
+const initStore = () => {
+    loadStore();
+    console.log('InitStore 🥑');
 }
+
+const loadStore = () => {
+    if( !localStorage.getItem('state') ) return;
+
+    const { todos = [], filter = Filters.All } = JSON.parse( localStorage.getItem('state') );
+    state.todos = todos;
+    state.filter = filter;
+}
+
+const saveStateToLocalStorage = () =>{
+    localStorage.setItem('state', JSON.stringify(state) );
+}
+
+
+const getTodos = ( filter = Filters.All ) => {
+    
+    switch( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if ( !description ) throw new Error('Description is required');
+    state.todos.push( new Todo(description) );
+
+    saveStateToLocalStorage();
+}
+
+/**
+ * 
+ * @param {String} todoId
+ */
+const toggleTodo = ( todoId ) => {
+    
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId ) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+
+    saveStateToLocalStorage();
+}
+
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !== todoId  );
+    saveStateToLocalStorage();
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => !todo.done );
+    saveStateToLocalStorage();
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter;
+    saveStateToLocalStorage();
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
--- 10-Todo-App/src/todos/models/todo.model.js
+++ 10-Todo-App/src/todos/models/todo.model.js
@@ -1,9 +1,11 @@
+import {v4 as uuid} from 'uuid'
+
 export class Todo {
     /**
      * @param {String} descripcion
      */
     constructor(description){
-        this.id = 1;
+        this.id = uuid();
         this.description = description;
         this.done = false;
         this.createdAt =new Date();
